a26ddc2273869d8074fa626d349b46389ad0256f
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-03-05 17:07:09 +0700

    Edit action

--- CityEvents/ViewModels/MainWindowViewModel.cs
+++ CityEvents/ViewModels/MainWindowViewModel.cs
@@ -1,34 +1,21 @@
 ﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using CityEvents.Models;
 
 namespace CityEvents.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private readonly ObservableCollection<CityEvent> _cItems;
+        public ObservableCollection<CityEvent> Events { get; } = new ObservableCollection<CityEvent>();
 
         public MainWindowViewModel()
         {
             CityEvent[] events = Serializer<CityEvent[]>.Load("../../../../events.xml");
-
-            _cItems = new ObservableCollection<CityEvent>(events);
-            _cItems.CollectionChanged += CItems_CollectionChanged!;
-        }
-
-        private void CItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Add || e.Action == NotifyCollectionChangedAction.Remove)
+            foreach (var e in events)
             {
-                // обновляем представление при добавлении/удалении элементов
+                Events.Add(e);
             }
-            else if (e.Action == NotifyCollectionChangedAction.Replace)
-            {
-                // обновляем представление при замене элементов
-            }
-        }
 
-        public ObservableCollection<CityEvent> CItems => _cItems;
+            Events.Add(new CityEvent());
+        }
     }
-
 }
